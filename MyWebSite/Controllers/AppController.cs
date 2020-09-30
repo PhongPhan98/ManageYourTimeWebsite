37715776c9cdd837cc279d827f72fc201aaf6f2a
phongphan <phongpx9x@gmail.com>
2020-09-30 15:21:22 +0700

    load form

--- MyWebSite/Controllers/AppController.cs
+++ MyWebSite/Controllers/AppController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MyWebSite.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +10,11 @@ namespace MyWebSite.Controllers
     public class AppController : Controller
     {
         // GET: App
-
+        private MyDBEntities2 db = new MyDBEntities2();
         [NoDirectAccess]
         public ActionResult Index()
         {
-            return View();
+            return View(db.ToChucHoTroes.ToList());
         }
     }
 }
